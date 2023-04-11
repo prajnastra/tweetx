46e08ddc96fcad302446327b1bd0661435317315
prajnastra <coder.abhijit90@gmail.com>
2023-04-11 10:55:36 +0530

    add: cors & drf

--- server/tweetx/settings.py
+++ server/tweetx/settings.py
@@ -46,10 +46,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "users",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
